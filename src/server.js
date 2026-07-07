006ae17b0e3f2a3e6942f1eb37e376320dce86a8
abhishek <ap7785980@gmail.com>
2026-07-07 20:26:54 +0530

    feat: initialize employee search API project

---
+++ src/server.js
@@ -0,0 +1,9 @@
+require("dotenv").config()
+const app= require("./app");
+const connectdb=require("./config/db");
+connectdb();
+app.listen(process.env.PORT,(req,res)=>{
+    console.log(`server start at port ${process.env.PORT}`)
+})
+
+
